01a9129d7a58508914c13fb1b04ae80b36fd877c
Moth <moth@localhost.localdomain>
2024-09-12 12:00:53 -0500

    Part 2 completed

--- hw2.cpp
+++ hw2.cpp
@@ -7,13 +7,32 @@ int main()
     bool isSunny = true;
     bool isRaining = false;
     bool isWeekend = true;
+    bool haveUmbrella = true;
+    bool isGoingOutside = true;
+    bool isTired = false;
+
     cout << "Sunny? " << isSunny << endl;
     cout << "Raining? " << isRaining << endl;
+
     if (isWeekend)
     {
         cout << "It's the weekend!" << endl;
     }
-    
+
+    if (isRaining && haveUmbrella)
+    {
+        cout << "You can go outside with an umbrella." << endl;
+    }
+
+    if (isSunny || isWeekend)
+    {
+        cout << "It's a great time to relax!" << endl;
+    }
+
+    if (!isTired)
+    {
+        cout << "You have energy to go for a walk!" << endl;
+    }
 
     return 0;
 }
