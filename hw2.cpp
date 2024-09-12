1b7e003f2e5887dbf7ef7ee61981710367f57cc2
Moth <moth@localhost.localdomain>
2024-09-12 11:54:37 -0500

    part 1 completed

--- hw2.cpp
+++ hw2.cpp
@@ -6,8 +6,14 @@ int main()
 {
     bool isSunny = true;
     bool isRaining = false;
+    bool isWeekend = true;
     cout << "Sunny? " << isSunny << endl;
     cout << "Raining? " << isRaining << endl;
+    if (isWeekend)
+    {
+        cout << "It's the weekend!" << endl;
+    }
+    
 
     return 0;
 }
